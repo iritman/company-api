9b5ba658b7b32fa5e0d2a60f5c790ede0ea03afa
Naiem YF <iritman@gmail.com>
2022-01-12 18:39:02 +0330

    Implement user-security-guard-reged-cards.js

--- startup/official-routes.js
+++ startup/official-routes.js
@@ -1,7 +1,15 @@
 const userDuties = require("./../routers/official/org/user-duties");
 const userMembersDuties = require("./../routers/official/org/user-members-duties");
+//---
+const useSecurityGuardRegedCards = require("./../routers/official/timex/user-security-guard-reged-cards");
+//---
 
 module.exports = function (app) {
   app.use("/api/official/org/user-duties", userDuties);
   app.use("/api/official/org/user-members-duties", userMembersDuties);
+  //---
+  app.use(
+    "/api/official/timex/user-security-guard-reged-cards",
+    useSecurityGuardRegedCards
+  );
 };
