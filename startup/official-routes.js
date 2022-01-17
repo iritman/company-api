3abd47cd7ddd76b6ae4e9780b4a62887a35f637a
Naiem YF <iritman@gmail.com>
2022-01-17 18:53:49 +0330

    Implement user-my-vacations.js

--- startup/official-routes.js
+++ startup/official-routes.js
@@ -4,6 +4,7 @@ const userMembersDuties = require("./../routers/official/org/user-members-duties
 const userSecurityGuardRegedCards = require("./../routers/official/timex/user-security-guard-reged-cards");
 const userMyRegedCards = require("./../routers/official/timex/user-my-reged-cards");
 const userMyWorkShifts = require("./../routers/official/timex/user-my-work-shifts");
+const userMyVacations = require("./../routers/official/timex/user-my-vacations");
 //---
 const userMembersRegedCards = require("./../routers/official/timex/user-members-reged-cards");
 const userMembersWorkShifts = require("./../routers/official/timex/user-members-work-shifts");
@@ -19,6 +20,7 @@ module.exports = function (app) {
   );
   app.use("/api/official/timex/user-my-reged-cards", userMyRegedCards);
   app.use("/api/official/timex/user-my-work-shifts", userMyWorkShifts);
+  app.use("/api/official/timex/user-my-vacations", userMyVacations);
   //---
   app.use(
     "/api/official/timex/user-members-reged-cards",
