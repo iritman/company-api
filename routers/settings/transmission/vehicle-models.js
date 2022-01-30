bc5ec71d72929c6eb6709c8f1a4516a8802263b6
Naiem YF <iritman@gmail.com>
2022-01-30 18:01:29 +0330

    Add 'params' API to vehicle-models.js

--- routers/settings/transmission/vehicle-models.js
+++ routers/settings/transmission/vehicle-models.js
@@ -3,6 +3,24 @@ const auth = require("../../../middlewares/auth");
 const router = express.Router();
 const { selectQuery } = require("../../../startup/db");
 
+router.get("/params", auth, async (req, res) => {
+  const { MemberID } = req.user;
+
+  let result = await selectQuery(
+    `EXEC TrasnmissionAPI.GetModelsParams ${MemberID}`
+  );
+
+  result = result.recordset[0];
+
+  if (result.Error) return res.status(400).send(result);
+
+  for (const key in result) {
+    result[key] = JSON.parse(result[key]);
+  }
+
+  res.send(result);
+});
+
 router.get("/", auth, async (req, res) => {
   const { MemberID } = req.user;
 
