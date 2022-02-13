1965f68d26337cbede5c53c19ce2865d39694e60
Naiem YF <iritman@gmail.com>
2022-02-13 20:15:34 +0330

    Implement Delete Report API in user-my-missions.js

--- routers/official/timex/user-my-missions.js
+++ routers/official/timex/user-my-missions.js
@@ -72,6 +72,22 @@ router.post("/", auth, async (req, res) => {
   res.send(result);
 });
 
+router.delete("/report/:recordID", auth, async (req, res) => {
+  const { MemberID } = req.user;
+
+  let result = await selectQuery(
+    `EXEC TimexAPI.DeleteMyMissionReport ${MemberID}, ${req.params.recordID}`
+  );
+
+  result = result.recordset[0];
+
+  if (result.Error) return res.status(400).send(result);
+
+  result.ReportInfo = JSON.parse(result.ReportInfo);
+
+  res.send(result);
+});
+
 router.delete("/:recordID", auth, async (req, res) => {
   const { MemberID } = req.user;
 
