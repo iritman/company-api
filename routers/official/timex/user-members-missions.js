c43fc7dc1bd80a62f3a8b54ccf99ecb0c49a63ff
Naiem YF <iritman@gmail.com>
2022-02-16 23:08:18 +0330

    Add Save & Delete Note APIs to user-members-missions.js

--- routers/official/timex/user-members-missions.js
+++ routers/official/timex/user-members-missions.js
@@ -32,11 +32,64 @@ router.post("/search", auth, async (req, res) => {
 
   result = result.recordset;
 
+  //---
+
   result.forEach((mission) => {
-    if (mission.VehicleInfo.length > 0)
+    if (mission.VehicleInfo.length > 0) {
       mission.VehicleInfo = JSON.parse(mission.VehicleInfo);
+    }
+
     mission.ReportInfo = JSON.parse(mission.ReportInfo);
+    mission.Notes = JSON.parse(mission.Notes);
   });
+
+  //---
+
+  res.send(result);
+});
+
+router.post("/note", auth, async (req, res) => {
+  const { MemberID } = req.user;
+
+  let result = await selectQuery(
+    `EXEC TimexAPI.SaveMissionNote ${MemberID}, N'${JSON.stringify(req.body)}'`
+  );
+
+  result = result.recordset[0];
+
+  if (result.Error) return res.status(400).send(result);
+
+  //---
+
+  if (result.VehicleInfo.length > 0) {
+    result.VehicleInfo = JSON.parse(result.VehicleInfo);
+  }
+
+  result.ReportInfo = JSON.parse(result.ReportInfo);
+  result.Notes = JSON.parse(result.Notes);
+
+  //---
+
+  res.send(result);
+});
+
+router.delete("/note/:recordID", auth, async (req, res) => {
+  const { MemberID } = req.user;
+
+  let result = await selectQuery(
+    `EXEC TimexAPI.DeleteMissionNote ${MemberID}, ${req.params.recordID}`
+  );
+
+  result = result.recordset[0];
+
+  if (result.Error) return res.status(400).send(result);
+
+  if (result.VehicleInfo.length > 0) {
+    result.VehicleInfo = JSON.parse(result.VehicleInfo);
+  }
+
+  result.ReportInfo = JSON.parse(result.ReportInfo);
+  result.Notes = JSON.parse(result.Notes);
 
   res.send(result);
 });
