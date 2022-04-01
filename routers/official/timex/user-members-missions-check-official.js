a0512e3b9b94427370baa1972db57dc48a78f6b1
Naiem YF <iritman@gmail.com>
2022-04-01 09:08:17 +0430

    Update APIs to handle Actions field in missions check official

--- routers/official/timex/user-members-missions-check-official.js
+++ routers/official/timex/user-members-missions-check-official.js
@@ -30,7 +30,21 @@ router.post("/search", auth, async (req, res) => {
     )}'`
   );
 
-  res.send(result.recordset);
+  result = result.recordset;
+
+  if (result.length === 1 && result[0].Error)
+    return res.status(400).send(result[0]);
+
+  result.forEach((mission) => {
+    if (mission.VehicleInfo.length > 0) {
+      mission.VehicleInfo = JSON.parse(mission.VehicleInfo);
+    }
+
+    mission.ReportInfo = JSON.parse(mission.ReportInfo);
+    mission.Actions = JSON.parse(mission.Actions);
+  });
+
+  res.send(result);
 });
 
 router.post("/response", auth, async (req, res) => {
@@ -45,6 +59,13 @@ router.post("/response", auth, async (req, res) => {
   result = result.recordset[0];
 
   if (result.Error) return res.status(400).send(result);
+
+  if (result.VehicleInfo.length > 0) {
+    result.VehicleInfo = JSON.parse(result.VehicleInfo);
+  }
+
+  result.ReportInfo = JSON.parse(result.ReportInfo);
+  result.Actions = JSON.parse(result.Actions);
 
   res.send(result);
 });
