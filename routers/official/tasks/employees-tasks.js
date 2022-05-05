28cbe46831e828c00241686d7fc743789900fa26
Naiem YF <iritman@gmail.com>
2022-05-05 21:52:09 +0430

    Update employees-tasks.js

--- routers/official/tasks/employees-tasks.js
+++ routers/official/tasks/employees-tasks.js
@@ -2,6 +2,7 @@ const express = require("express");
 const auth = require("../../../middlewares/auth");
 const router = express.Router();
 const { selectQuery } = require("../../../startup/db");
+const fs = require("fs");
 
 router.get("/params", auth, async (req, res) => {
   const { MemberID } = req.user;
@@ -21,18 +22,29 @@ router.get("/params", auth, async (req, res) => {
   res.send(result);
 });
 
-// router.get("/", auth, async (req, res) => {
-//   const { MemberID } = req.user;
+router.get("/", auth, async (req, res) => {
+  const { MemberID } = req.user;
 
-//   let result = await selectQuery(`EXEC TaskAPI.GetAllTags ${MemberID}`);
+  let result = await selectQuery(`EXEC TaskAPI.GetEmployeesTasks ${MemberID}`);
 
-//   result = result.recordset;
+  result = result.recordset;
 
-//   if (result.length === 1 && result[0].Error)
-//     return res.status(400).send(result[0]);
+  if (result.length === 1 && result[0].Error)
+    return res.status(400).send(result[0]);
 
-//   res.send(result);
-// });
+  result.forEach((task) => {
+    task.Tags = JSON.parse(task.Tags);
+    task.Reports = JSON.parse(task.Reports);
+    task.Reports.forEach((report) => {
+      report.TaskReportSeens = JSON.parse(report.TaskReportSeens);
+    });
+    task.Supervisors = JSON.parse(task.Supervisors);
+    task.Files = JSON.parse(task.Files);
+    task.ReminderInfo = JSON.parse(task.ReminderInfo);
+  });
+
+  res.send(result);
+});
 
 // router.post("/search", auth, async (req, res) => {
 //   const { searchText } = req.body;
@@ -61,11 +73,14 @@ router.post("/", auth, async (req, res) => {
 
   if (result.Error) return res.status(400).send(result);
 
-  result.TaskTags = JSON.parse(result.TaskTags);
-  result.TaskReports = JSON.parse(result.TaskReports);
-  result.TaskReportSeens = JSON.parse(result.TaskReportSeens);
-  result.TaskSupervisors = JSON.parse(result.TaskSupervisors);
-  result.TaskFiles = JSON.parse(result.TaskFiles);
+  result.Tags = JSON.parse(result.Tags);
+  result.Reports = JSON.parse(result.Reports);
+  result.Reports.forEach((report) => {
+    report.TaskReportSeens = JSON.parse(report.TaskReportSeens);
+  });
+  result.Supervisors = JSON.parse(result.Supervisors);
+  result.Files = JSON.parse(result.Files);
+  result.ReminderInfo = JSON.parse(result.ReminderInfo);
 
   res.send(result);
 });
@@ -81,7 +96,22 @@ router.delete("/:recordID", auth, async (req, res) => {
 
   if (result.Error) return res.status(400).send(result);
 
-  res.send(result);
+  result.DeletedFiles = JSON.parse(result.DeletedFiles);
+
+  const baseDir = `./uploaded-files/`;
+  let dir = "";
+
+  result.DeletedFiles.forEach((f) => {
+    dir = `${baseDir}${f.TypeID === 1 ? "task-files" : "task-report-files"}/${
+      f.FileName
+    }`;
+
+    if (fs.existsSync(dir)) {
+      fs.unlinkSync(dir);
+    }
+  });
+
+  res.send({ Message: result.Message });
 });
 
 module.exports = router;
