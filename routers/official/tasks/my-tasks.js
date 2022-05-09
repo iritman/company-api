d4539868e22c5d462eb469e4355a4e00af04f5d7
Naiem YF <iritman@gmail.com>
2022-05-09 19:26:07 +0430

    Implement my done tasks APIs

--- routers/official/tasks/my-tasks.js
+++ routers/official/tasks/my-tasks.js
@@ -4,23 +4,23 @@ const router = express.Router();
 const { selectQuery } = require("../../../startup/db");
 const fs = require("fs");
 
-// router.get("/params", auth, async (req, res) => {
-//   const { MemberID } = req.user;
+router.get("/my/done/params", auth, async (req, res) => {
+  const { MemberID } = req.user;
 
-//   let result = await selectQuery(
-//     `EXEC TaskAPI.GetEmployeesTasksParams ${MemberID}`
-//   );
+  let result = await selectQuery(
+    `EXEC TaskAPI.GetSearchMyDoneTasksParams ${MemberID}`
+  );
 
-//   result = result.recordset[0];
+  result = result.recordset[0];
 
-//   if (result.Error) return res.status(400).send(result);
+  if (result.Error) return res.status(400).send(result);
 
-//   for (const key in result) {
-//     result[key] = JSON.parse(result[key]);
-//   }
+  for (const key in result) {
+    result[key] = JSON.parse(result[key]);
+  }
 
-//   res.send(result);
-// });
+  res.send(result);
+});
 
 // router.get("/files/:taskID", auth, async (req, res) => {
 //   const { MemberID } = req.user;
@@ -73,45 +73,56 @@ router.post("/task/seen/:taskID", auth, async (req, res) => {
   res.send(result);
 });
 
-// router.post("/search", auth, async (req, res) => {
-//   const { searchText } = req.body;
-//   const { MemberID } = req.user;
+router.post("/search", auth, async (req, res) => {
+  const { MemberID } = req.user;
 
-//   let result = await selectQuery(
-//     `EXEC TaskAPI.SearchTags ${MemberID}, N'${searchText}'`
-//   );
+  let result = await selectQuery(
+    `EXEC TaskAPI.SearchMyDoneTasks ${MemberID}, N'${JSON.stringify(req.body)}'`
+  );
 
-//   result = result.recordset;
+  result = result.recordset;
 
-//   if (result.length === 1 && result[0].Error)
-//     return res.status(400).send(result[0]);
+  if (result.length === 1 && result[0].Error)
+    return res.status(400).send(result[0]);
 
-//   res.send(result);
-// });
+  result.forEach((task) => {
+    task.Tags = JSON.parse(task.Tags);
+    task.Reports = JSON.parse(task.Reports);
+    task.Reports.forEach((report) => {
+      report.TaskReportSeens = JSON.parse(report.TaskReportSeens);
+      report.Files = JSON.parse(report.Files);
+    });
+    task.Supervisors = JSON.parse(task.Supervisors);
+    task.Files = JSON.parse(task.Files);
+    task.ReminderInfo = JSON.parse(task.ReminderInfo);
+  });
 
-// router.post("/", auth, async (req, res) => {
-//   const { MemberID } = req.user;
+  res.send(result);
+});
 
-//   let result = await selectQuery(
-//     `EXEC TaskAPI.SaveEmployeeTask ${MemberID}, N'${JSON.stringify(req.body)}'`
-//   );
+router.post("/", auth, async (req, res) => {
+  const { MemberID } = req.user;
 
-//   result = result.recordset[0];
+  let result = await selectQuery(
+    `EXEC TaskAPI.SaveEmployeeTask ${MemberID}, N'${JSON.stringify(req.body)}'`
+  );
 
-//   if (result.Error) return res.status(400).send(result);
+  result = result.recordset[0];
 
-//   result.Tags = JSON.parse(result.Tags);
-//   result.Reports = JSON.parse(result.Reports);
-//   result.Reports.forEach((report) => {
-//     report.TaskReportSeens = JSON.parse(report.TaskReportSeens);
-//     report.Files = JSON.parse(report.Files);
-//   });
-//   result.Supervisors = JSON.parse(result.Supervisors);
-//   result.Files = JSON.parse(result.Files);
-//   result.ReminderInfo = JSON.parse(result.ReminderInfo);
+  if (result.Error) return res.status(400).send(result);
 
-//   res.send(result);
-// });
+  result.Tags = JSON.parse(result.Tags);
+  result.Reports = JSON.parse(result.Reports);
+  result.Reports.forEach((report) => {
+    report.TaskReportSeens = JSON.parse(report.TaskReportSeens);
+    report.Files = JSON.parse(report.Files);
+  });
+  result.Supervisors = JSON.parse(result.Supervisors);
+  result.Files = JSON.parse(result.Files);
+  result.ReminderInfo = JSON.parse(result.ReminderInfo);
+
+  res.send(result);
+});
 
 // router.delete("/:recordID", auth, async (req, res) => {
 //   const { MemberID } = req.user;
