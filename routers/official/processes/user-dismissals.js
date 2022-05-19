d603b51e96481082df36522194b08ac0c02bc643
Naiem YF <iritman@gmail.com>
2022-05-19 11:46:13 +0430

    Implement reg new dismissal report API for official expert

--- routers/official/processes/user-dismissals.js
+++ routers/official/processes/user-dismissals.js
@@ -37,6 +37,9 @@ router.post("/search", auth, async (req, res) => {
 
   result.forEach((dismissal) => {
     dismissal.Reports = JSON.parse(dismissal.Reports);
+    dismissal.Reports.forEach(
+      (report) => (report.Files = JSON.parse(report.Files))
+    );
     dismissal.Actions = JSON.parse(dismissal.Actions);
     dismissal.Files = JSON.parse(dismissal.Files);
   });
@@ -56,6 +59,7 @@ router.post("/", auth, async (req, res) => {
   if (result.Error) return res.status(400).send(result);
 
   result.Reports = JSON.parse(result.Reports);
+  result.Reports.forEach((report) => (report.Files = JSON.parse(report.Files)));
   result.Actions = JSON.parse(result.Actions);
   result.Files = JSON.parse(result.Files);
 
@@ -75,55 +79,5 @@ router.delete("/:recordID", auth, async (req, res) => {
 
   res.send(result);
 });
-
-// router.post("/report", auth, async (req, res) => {
-//   const { MemberID } = req.user;
-
-//   let result = await selectQuery(
-//     `EXEC ProcessAPI.SaveDismissalReport ${MemberID}, N'${JSON.stringify(
-//       req.body
-//     )}'`
-//   );
-
-//   result = result.recordset[0];
-
-//   if (result.Error) return res.status(400).send(result);
-
-//   //---
-
-//   if (result.VehicleInfo.length > 0) {
-//     result.VehicleInfo = JSON.parse(result.VehicleInfo);
-//   }
-
-//   result.ReportInfo = JSON.parse(result.ReportInfo);
-//   result.Notes = JSON.parse(result.Notes);
-//   result.Actions = JSON.parse(result.Actions);
-
-//   //---
-
-//   res.send(result);
-// });
-
-// router.delete("/report/:recordID", auth, async (req, res) => {
-//   const { MemberID } = req.user;
-
-//   let result = await selectQuery(
-//     `EXEC ProcessAPI.DeleteDismissalReport ${MemberID}, ${req.params.recordID}`
-//   );
-
-//   result = result.recordset[0];
-
-//   if (result.Error) return res.status(400).send(result);
-
-//   if (result.VehicleInfo.length > 0) {
-//     result.VehicleInfo = JSON.parse(result.VehicleInfo);
-//   }
-
-//   result.ReportInfo = JSON.parse(result.ReportInfo);
-//   result.Notes = JSON.parse(result.Notes);
-//   result.Actions = JSON.parse(result.Actions);
-
-//   res.send(result);
-// });
 
 module.exports = router;
--- routers/official/processes/user-official-check-dismissals.js
+++ routers/official/processes/user-official-check-dismissals.js
@@ -37,6 +37,9 @@ router.post("/search", auth, async (req, res) => {
 
   result.forEach((dismissal) => {
     dismissal.Reports = JSON.parse(dismissal.Reports);
+    dismissal.Reports.forEach(
+      (report) => (report.Files = JSON.parse(report.Files))
+    );
     dismissal.Actions = JSON.parse(dismissal.Actions);
     dismissal.Files = JSON.parse(dismissal.Files);
   });
@@ -58,10 +61,55 @@ router.post("/response", auth, async (req, res) => {
   if (result.Error) return res.status(400).send(result);
 
   result.Reports = JSON.parse(result.Reports);
+  result.Reports.forEach((report) => (report.Files = JSON.parse(report.Files)));
   result.Actions = JSON.parse(result.Actions);
   result.Files = JSON.parse(result.Files);
 
   res.send(result);
 });
+
+router.post("/report", auth, async (req, res) => {
+  const { MemberID } = req.user;
+
+  let result = await selectQuery(
+    `EXEC ProcessAPI.SaveDismissalOfficialReport ${MemberID}, N'${JSON.stringify(
+      req.body
+    )}'`
+  );
+
+  result = result.recordset[0];
+
+  if (result.Error) return res.status(400).send(result);
+
+  //---
+
+  result.Files = JSON.parse(result.Files);
+
+  //---
+
+  res.send(result);
+});
+
+// router.delete("/report/:recordID", auth, async (req, res) => {
+//   const { MemberID } = req.user;
+
+//   let result = await selectQuery(
+//     `EXEC ProcessAPI.DeleteDismissalReport ${MemberID}, ${req.params.recordID}`
+//   );
+
+//   result = result.recordset[0];
+
+//   if (result.Error) return res.status(400).send(result);
+
+//   if (result.VehicleInfo.length > 0) {
+//     result.VehicleInfo = JSON.parse(result.VehicleInfo);
+//   }
+
+//   result.ReportInfo = JSON.parse(result.ReportInfo);
+//   result.Notes = JSON.parse(result.Notes);
+//   result.Actions = JSON.parse(result.Actions);
+
+//   res.send(result);
+// });
 
 module.exports = router;
