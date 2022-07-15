e7cc8c2f725262279fd689463d7e196aa11c8c0f
Naiem YF <iritman@gmail.com>
2022-07-16 02:06:33 +0430

    Implement user-product-natures.js

--- startup/financial-routes.js
+++ startup/financial-routes.js
@@ -1,6 +1,8 @@
 const userStores = require("./../routers/financial/store-mgr/user-stores");
+const userProductNatures = require("./../routers/financial/store-mgr/user-product-natures");
 //---
 
 module.exports = function (app) {
   app.use("/api/financial/store-mgr/user-stores", userStores);
+  app.use("/api/financial/store-mgr/user-product-natures", userProductNatures);
 };
