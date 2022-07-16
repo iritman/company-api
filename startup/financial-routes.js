712e191fd5f17f59f3687b86b8f39bbe460ce9b2
Naiem YF <iritman@gmail.com>
2022-07-16 23:15:13 +0430

    Implement user-measure-types.js

--- startup/financial-routes.js
+++ startup/financial-routes.js
@@ -1,8 +1,10 @@
 const userStores = require("./../routers/financial/store-mgr/user-stores");
 const userProductNatures = require("./../routers/financial/store-mgr/user-product-natures");
+const userMesureTypes = require("./../routers/financial/store-mgr/user-measure-types");
 //---
 
 module.exports = function (app) {
   app.use("/api/financial/store-mgr/user-stores", userStores);
   app.use("/api/financial/store-mgr/user-product-natures", userProductNatures);
+  app.use("/api/financial/store-mgr/user-measure-types", userMesureTypes);
 };
