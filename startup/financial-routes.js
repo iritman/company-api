83b669c181265d18f27d47f764d88234982e08cd
Naiem YF <iritman@gmail.com>
2022-07-17 00:44:43 +0430

    Implement user-pricing-types.js

--- startup/financial-routes.js
+++ startup/financial-routes.js
@@ -2,6 +2,7 @@ const userStores = require("./../routers/financial/store-mgr/user-stores");
 const userProductNatures = require("./../routers/financial/store-mgr/user-product-natures");
 const userMesureTypes = require("./../routers/financial/store-mgr/user-measure-types");
 const userMesureUnits = require("./../routers/financial/store-mgr/user-measure-units");
+const userPricingTypes = require("./../routers/financial/store-mgr/user-pricing-types");
 //---
 
 module.exports = function (app) {
@@ -9,4 +10,5 @@ module.exports = function (app) {
   app.use("/api/financial/store-mgr/user-product-natures", userProductNatures);
   app.use("/api/financial/store-mgr/user-measure-types", userMesureTypes);
   app.use("/api/financial/store-mgr/user-measure-units", userMesureUnits);
+  app.use("/api/financial/store-mgr/user-pricing-types", userPricingTypes);
 };
