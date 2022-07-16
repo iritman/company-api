d5e71f1c55a45f456e656af6c8601fe4db56c97d
Naiem YF <iritman@gmail.com>
2022-07-17 00:32:26 +0430

    Add measure units route to financial routes

--- startup/financial-routes.js
+++ startup/financial-routes.js
@@ -1,10 +1,12 @@
 const userStores = require("./../routers/financial/store-mgr/user-stores");
 const userProductNatures = require("./../routers/financial/store-mgr/user-product-natures");
 const userMesureTypes = require("./../routers/financial/store-mgr/user-measure-types");
+const userMesureUnits = require("./../routers/financial/store-mgr/user-measure-units");
 //---
 
 module.exports = function (app) {
   app.use("/api/financial/store-mgr/user-stores", userStores);
   app.use("/api/financial/store-mgr/user-product-natures", userProductNatures);
   app.use("/api/financial/store-mgr/user-measure-types", userMesureTypes);
+  app.use("/api/financial/store-mgr/user-measure-units", userMesureUnits);
 };
