65e8bdc9fa4a6da1e70fe293087f5f6d797c263b
Naiem YF <iritman@gmail.com>
2022-09-04 20:55:41 +0430

    implement projects.js

--- startup/financial-routes.js
+++ startup/financial-routes.js
@@ -1,3 +1,5 @@
+const projects = require("./../routers/financial/public-settings/projects");
+//---
 const userStores = require("./../routers/financial/store-mgr/user-stores");
 const userProductNatures = require("./../routers/financial/store-mgr/user-product-natures");
 const userMesureTypes = require("./../routers/financial/store-mgr/user-measure-types");
@@ -12,6 +14,8 @@ const userBaches = require("./../routers/financial/store-mgr/user-baches");
 //---
 
 module.exports = function (app) {
+  app.use("/api/financial/public-settings/projects", projects);
+  //---
   app.use("/api/financial/store-mgr/user-stores", userStores);
   app.use("/api/financial/store-mgr/user-product-natures", userProductNatures);
   app.use("/api/financial/store-mgr/user-measure-types", userMesureTypes);
@@ -29,4 +33,5 @@ module.exports = function (app) {
   app.use("/api/financial/store-mgr/user-products", userProducts);
   app.use("/api/financial/store-mgr/user-bach-patterns", userBachPatterns);
   app.use("/api/financial/store-mgr/user-baches", userBaches);
+  //---
 };
