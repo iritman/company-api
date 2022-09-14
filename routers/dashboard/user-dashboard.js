dc653cb9cb729ac69d1389dcb4e5f42f795c2a48
Naiem YF <iritman@gmail.com>
2022-09-15 01:55:29 +0430

    Implement two new APIs

--- routers/dashboard/user-dashboard.js
+++ routers/dashboard/user-dashboard.js
@@ -33,4 +33,43 @@ router.get("/task", auth, async (req, res) => {
   res.send(result);
 });
 
+router.get(
+  "/task/:departmentID/:calculateSubDepartments",
+  auth,
+  async (req, res) => {
+    const { MemberID } = req.user;
+    const { departmentID, calculateSubDepartments } = req.params;
+
+    let result = await selectQuery(
+      `EXEC TaskAPI.GetDepartmentTasksDashboardStatistics ${MemberID}, ${departmentID}, ${
+        calculateSubDepartments === true
+      }`
+    );
+
+    result = result.recordset[0];
+
+    if (result.Error) return res.status(400).send(result);
+
+    result.MyColleagues = JSON.parse(result.MyColleagues);
+
+    res.send(result);
+  }
+);
+
+router.get("/task/departments", auth, async (req, res) => {
+  const { MemberID } = req.user;
+
+  let result = await selectQuery(
+    `EXEC TaskAPI.GetMemberDepartmentInfo ${MemberID}`
+  );
+
+  result = result.recordset[0];
+
+  if (result.Message) return res.send(result);
+
+  result.Departments = JSON.parse(result.Departments);
+
+  res.send(result);
+});
+
 module.exports = router;
