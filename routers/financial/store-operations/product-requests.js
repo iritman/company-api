c6d0c8c498894e86c005ed4316541e2922140050
iritman <iritman@gmail.com>
2024-02-11 12:49:26 +0330

    Remove get search params API from product-requests.js

--- routers/financial/store-operations/product-requests.js
+++ routers/financial/store-operations/product-requests.js
@@ -3,60 +3,11 @@ const auth = require("../../../middlewares/auth");
 const router = express.Router();
 const { selectQuery } = require("../../../startup/db");
 
-router.get("/form/ui/request", auth, async (req, res) => {
-  // const { MemberID } = req.user;
-
-  let result = await selectQuery(`EXEC AppAPI.GetFormUI 1`);
-
-  result = result.recordset[0];
-
-  if (result.Error) return res.status(400).send(result);
-
-  result.FormItems = JSON.parse(result.FormItems);
-  result.FormItems.forEach((item) => {
-    item.DataSource = JSON.parse(item.DataSource);
-
-    if (item.DataSource.length > 0) {
-      const first_row = item.DataSource[0];
-      const data_source_key_field = Object.keys(first_row)[0];
-
-      // Masalan agar 2 field <FromStoreID> va <ToStoreID> darim ke dar db
-      // faghat sotune <StoreID> hast,
-      // Ba in ravesh, field morede niaz be data source add mishavad
-      if (data_source_key_field !== item.FieldName) {
-        item.DataSource.forEach((ds_item) => {
-          ds_item[item.FieldName] = ds_item[data_source_key_field];
-        });
-      }
-    }
-  });
-
-  res.send(result);
-});
-
 router.get("/params", auth, async (req, res) => {
   const { MemberID } = req.user;
 
   let result = await selectQuery(
     `EXEC Financial_StoreOprAPI.GetProductRequestParams ${MemberID}`
-  );
-
-  result = result.recordset[0];
-
-  if (result.Error) return res.status(400).send(result);
-
-  for (const key in result) {
-    result[key] = JSON.parse(result[key]);
-  }
-
-  res.send(result);
-});
-
-router.get("/search/params", auth, async (req, res) => {
-  const { MemberID } = req.user;
-
-  let result = await selectQuery(
-    `EXEC Financial_StoreOprAPI.GetProductRequestSearchParams ${MemberID}`
   );
 
   result = result.recordset[0];
